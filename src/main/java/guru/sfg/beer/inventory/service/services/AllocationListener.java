7ab6d34c0702ca2522707f2213cf29cf03a22054
John Thompson <john@springframework.guru>
2019-12-03 19:59:22 -0500

    handle allocation request - closes #12

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,45 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.brewery.model.events.AllocateOrderRequest;
+import guru.sfg.brewery.model.events.AllocateOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 12/3/19.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateOrderRequest request){
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try{
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            if (allocationResult){
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+        } catch (Exception e){
+            log.error("Allocation failed for Order Id:" + request.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE,
+                builder.build());
+
+    }
+
+}
