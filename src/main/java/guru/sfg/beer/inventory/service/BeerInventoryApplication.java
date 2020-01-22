4e81beb23b1fc6e9715a43deed1a3bf9182554ee
Luke <kemnerluke@gmail.com>
2020-01-22 17:59:11 -0500

    enable artemisAutoConfig

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
+@SpringBootApplication
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
