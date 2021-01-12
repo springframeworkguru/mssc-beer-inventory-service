8dbf7284160247f229145fbb42fa8f62b54e6993
luorenjie <lrj193927@gmail.com>
2021-01-12 17:04:41 -0500

    update to use mysql

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,8 +2,9 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
