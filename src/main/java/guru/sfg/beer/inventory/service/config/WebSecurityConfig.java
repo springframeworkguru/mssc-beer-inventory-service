dc5026afd6eb582ec7763b5cd95defeab50e5453
Fredohm <fredwaucquez@hotmail.fr>
2021-05-03 06:42:50 +0200

    Docker Compose and ELK Stack

--- src/main/java/guru/sfg/beer/inventory/service/config/WebSecurityConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/WebSecurityConfig.java
@@ -1,19 +1,19 @@
-package guru.sfg.beer.inventory.service.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-}
+//package guru.sfg.beer.inventory.service.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable()
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+//    }
+//}
