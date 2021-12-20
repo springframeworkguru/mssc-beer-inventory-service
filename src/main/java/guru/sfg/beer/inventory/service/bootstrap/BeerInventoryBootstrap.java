b61dddc805cb2db8140a17c151e3e0283bba33ae
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-20 15:41:38 +0700

    Added aws mysql profile to utilize aws mysql instead of embedded H2.

--- src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
+++ src/main/java/guru/sfg/beer/inventory/service/bootstrap/BeerInventoryBootstrap.java
@@ -73,7 +73,7 @@ public class BeerInventoryBootstrap implements CommandLineRunner {
     public void bootstrap2() {
         try {
             StringBuilder sb = new StringBuilder();
-            Files.readAllLines(Path.of("src/main/resources/data.txt")).forEach(sb::append);
+            Files.readAllLines(Path.of("src/main/resources/data.sql")).forEach(sb::append);
             em.createNativeQuery(sb.toString()).executeUpdate();
         } catch (IOException e) {
             e.printStackTrace();
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `beerinventoryservice`.`beer_inventory` (id, created_date, last_modified_date, quantity_on_hand, upc, version, beer_id) values
+('0a818933-087d-47f2-ad83-2f986ed087eb',  CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12, '0631234200036', 1,'0a818933-087d-47f2-ad83-2f986ed087eb'),
+('a712d914-61ea-4623-8bd0-32c0f6545bfd', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12, '0631234300019', 1, 'a712d914-61ea-4623-8bd0-32c0f6545bfd'),
+('026cc3c8-3a0c-4083-a05b-e908048c1b08', CURRENT_TIMESTAMP , CURRENT_TIMESTAMP , 12, '0083783375213', 1, '026cc3c8-3a0c-4083-a05b-e908048c1b08');
