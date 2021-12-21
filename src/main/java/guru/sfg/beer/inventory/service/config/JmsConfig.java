0076e73dd94ed2850d96d54306afb3141854c66e
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-21 14:43:35 +0700

    Configured JMS-support

---
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -0,0 +1,18 @@
+package guru.sfg.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    @Bean
+    MessageConverter messageConverter(){
+        MappingJackson2MessageConverter messageConverter = new MappingJackson2MessageConverter();
+        messageConverter.setTargetType(MessageType.TEXT);
+        messageConverter.setTypeIdPropertyName("_type");
+        return messageConverter;
+    }
+}
