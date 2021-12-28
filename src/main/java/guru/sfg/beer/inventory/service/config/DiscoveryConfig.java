e3339d89c2e0dcf137d767b14ebcbb317e2c27f8
LevPhilippov <filippov.l.a.89@gmail.com>
2021-12-28 17:43:29 +0700

    Added discovery client config.

---
+++ src/main/java/guru/sfg/beer/inventory/service/config/DiscoveryConfig.java
@@ -0,0 +1,11 @@
+package guru.sfg.beer.inventory.service.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableDiscoveryClient
+@Profile("local-discovery")
+public class DiscoveryConfig {
+}
